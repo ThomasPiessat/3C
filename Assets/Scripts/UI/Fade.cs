2dde7d1db5ff7a5e50505fdf154005e5c12f57f9
Thomas Piessat <thomasdidier30@gmail.com>
2020-04-20 12:03:45 +0200

    Add Fade
    Press 'M' to fade

---
+++ Assets/Scripts/UI/Fade.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    private Animator m_Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            ActiveFade();
+        }
+    }
+
+    private void ActiveFade()
+    {
+        m_Animator.SetTrigger("FadeOut");
+    }
+}
